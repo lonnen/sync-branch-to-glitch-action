5a64279202c4fbff16fef3ac6ed5e4a1133cf10f
lonnen <chris.lonnen@gmail.com>
2024-01-17 23:29:11 -0800

    improve checks on inputs
    If GLITCH_GIT_URL isn't set, display a message and bail. IF the target_branch is not set, assume they want to use main

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 set -e
 
-GGU="$GLITCH_GIT_URL"
+GGU=${GLITCH_GIT_URL:?"Missing Glitch Git URL in Environment. Please check your yml file"}
 
 GITHUB_TOKEN_AND_REPO_URL="https://github-actions:${TOKEN}@github.com/${GITHUB_REPOSITORY}"
 
@@ -9,4 +9,4 @@ git clone "${GITHUB_TOKEN_AND_REPO_URL}".git ghrepo
 cd ghrepo
 git remote add glitch "${GGU}"
 
-git push glitch "${TARGET_BRANCH}":main
+git push glitch "${TARGET_BRANCH:=main}":main
