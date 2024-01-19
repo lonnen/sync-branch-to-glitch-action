c09208f11b7c472d876f11ae860cb309dfb4d158
lonnen <chris.lonnen@gmail.com>
2024-01-19 01:00:08 -0800

    make token naming consistent
    TOKEN is undefined. GITHUB_TOKEN is the name of the variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 GGU=${GLITCH_GIT_URL:?"Missing Glitch Git URL in Environment. Please check your yml file"}
 
-GITHUB_TOKEN_AND_REPO_URL="https://github-actions:${TOKEN}@github.com/${GITHUB_REPOSITORY}"
+GITHUB_TOKEN_AND_REPO_URL="https://github-actions:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}"
 
 git clone "${GITHUB_TOKEN_AND_REPO_URL}".git ghrepo
 cd ghrepo
